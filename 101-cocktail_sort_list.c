d0ac27d9b5734a79c78cd9367f6ffa07349e5da6
hawi-claire <7hawi7@gmail.com>
2024-02-20 13:07:00 -0800

    Cocktail shaker sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,4 +1,4 @@
-nclude "sort.h"
+#include "sort.h"
 
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
